115f92d6aaf8d85ff6304293848279623039db44
JosiasNetto <josiascaldasnetto08@gmail.com>
2023-11-09 22:49:02 -0300

    Questao 10 Finalizada

--- Listas/Lista_01/i_club_penguin.py
+++ Listas/Lista_01/i_club_penguin.py
@@ -14,9 +14,10 @@ if numero == 1:
     
     elif len(string_3) > len(string_2) and len(string_3) > len(string_1):
         lugar_sinal = string_3
-    
-    elif len(string_1) == len(string_2) and len(string_1) == len(string_3):
-        print("Droga! Ainda não consegui descobrir o local que possui mais sinais desconhecidos! Vou ter que ficar mais um tempo nessa Mansão Mal-Assombrada...")
+
+    #Aqui se usa o or, pois, pode-se ter dois locais maiores que outro, mas, iguais entre si
+    elif len(string_1) == len(string_2) or len(string_1) == len(string_3) or len(string_2 == string_3):
+        print("(Droga! Ainda não consegui descobrir o local que possui mais sinais desconhecidos! Vou ter que ficar mais um tempo nessa Mansão Mal-Assombrada...)")
         achou_lugar = False
 
     #Número 2 muda o local para a string com o menor número de letras
@@ -30,7 +31,7 @@ elif numero == 2:
     elif len(string_3) < len(string_2) and len(string_3) < len(string_1):
         lugar_sinal = string_3
     
-    elif len(string_1) == len(string_2) and len(string_1) == len(string_3):
+    elif len(string_1) == len(string_2) or len(string_1) == len(string_3) or len(string_2) == len(string_3):
         print("(Droga! Ainda não consegui descobrir o local que possui mais sinais desconhecidos! Vou ter que ficar mais um tempo nessa Mansão Mal-Assombrada...)")
         achou_lugar = False
 
@@ -42,20 +43,20 @@ if not achou_lugar:
     sorted({string_1,string_2,string_3})
     if string_1 > string_2 and string_1 > string_3:
         lugar_sinal = string_1
-        achou_lugar == True
+        achou_lugar = True
         print(lugar_sinal)
 
     elif string_2 > string_1 and string_2 > string_3:
         lugar_sinal = string_2
-        achou_lugar == True
+        achou_lugar = True
         print(lugar_sinal)
 
     elif string_3 > string_2 and string_3 > string_1:
         lugar_sinal = string_3
-        achou_lugar == True
+        achou_lugar = True
         print(lugar_sinal)
 
-    elif string_1 == string_2 and string_1 == string_3:
+    elif string_1 == string_2 or string_1 == string_3 or string_2 == string_3:
         print("\"AAAAAA! Um fantasma me assustou!\"")
         print("(Uma mensagem apareceu no monitor que você estava usando. \"Agente, um erro inesperado aconteceu. A EPF contactará você novamente quando tudo estiver funcionando da forma correta. Nosso sistema foi invadido por alguém que se identifica como Hubert P.Enguin\")")
 #Achado o local com mais sinais, imprimi a mensagem
--- Listas/Lista_01/j_alice_boderlands.py
+++ Listas/Lista_01/j_alice_boderlands.py
@@ -2,6 +2,12 @@
 acertou_todas = True
 vivo = True
 pontos = int(0)
+#Variaveis para dizer no final se acertou ou nao
+acertou1 = str()    
+acertou2 = str()
+acertou3 = str()
+acertou4 = str()
+acertou5 = str()
 
 #Recebendo as caracteristicas da primeira porta, calculando se acerta ou não(dependendo do número) e modificando a pontuação
 direcao_escolhida = (input())
@@ -9,8 +15,11 @@ numero = int(input())
 
 if (numero % 2 == 0 and direcao_escolhida == "esquerda") or (numero % 2 == 1 and direcao_escolhida == "direita"):
         pontos += 150
+        acertou1 = "CERTA"
 else:
     pontos -= 150
+    acertou1 = "ERRADA"
+    acertou_todas = False
 
 #Segunda Porta
 direcao_escolhida = input()
@@ -18,13 +27,14 @@ cor = input()
 planta = input()
 macaneta = input()
 
-if ((cor == "dourada" or cor == "prateada") or ((planta == "avenca" or planta == "espadinha") and macaneta == "redonda")) and direcao_escolhida == "direita":
-    pontos += 150
-elif not ((cor == "dourada" or cor == "prateada") or ((planta == "avenca" or planta == "espadinha") and macaneta == "redonda")) and direcao_escolhida == "esquerda":
-    pontos += 250
-
+if (((cor == "dourada" or cor == "prateada") or ((planta == "avenca" or planta == "espadinha") and macaneta == "redonda")) and direcao_escolhida == "direita") or (not ((cor == "dourada" or cor == "prateada") or ((planta == "avenca" or planta == "espadinha") and macaneta == "redonda")) and direcao_escolhida == "esquerda"):
+    pontos += 200
+    acertou2 = "CERTA"
+   
 else:
-    pontos -= 250
+    pontos -= 200
+    acertou2 = "ERRADA"
+    acertou_todas = False
 
 #Terceira Porta
 direcao_escolhida = input()
@@ -33,23 +43,27 @@ numero = int(input())
 planta = input()
 macaneta = input()
 
-if (numero % 5 == 0 and planta == "espadinha" and macaneta == "quadrada") and direcao_escolhida == "esquerda":
+if (((numero % 5 == 0 and planta == "espadinha" and macaneta == "quadrada") or cor == "perolada") and direcao_escolhida == "esquerda") or (not (numero % 5 == 0 and planta == "espadinha" and macaneta == "quadrada") and direcao_escolhida == "direita"):
     pontos += 250
-elif not (numero % 5 == 0 and planta == "espadinha" and macaneta == "quadrada") and direcao_escolhida == "direita":
-    pontos += 250
+    acertou3 = "CERTA"
+
 else:
     pontos -= 250
+    acertou3 = "ERRADA"
+    acertou_todas = False
 
 #Quarta Porta
 direcao_escolhida = input()
 numero = int(input())
 
-if (numero % 3 == 0) and not(numero % 5 == 0 and numero % 2 == 0) and direcao_escolhida == "direita":
+if ((numero % 3 == 0) and not(numero % 5 == 0 and numero % 2 == 0) and direcao_escolhida == "direita") or (not((numero % 3 == 0) and not(numero % 5 == 0 and numero % 2 == 0)) and direcao_escolhida == "esquerda"):
     pontos += 300
-elif  not((numero % 3 == 0) and not(numero % 5 == 0 and numero % 2 == 0)) and direcao_escolhida == "esquerda":
-    pontos += 300
+    acertou4 = "CERTA"
+    
 else:
     pontos -= 300
+    acertou4 = "ERRADA"
+    acertou_todas = False
 
 #Quinta Porta
 cor = input()
@@ -60,15 +74,34 @@ macaneta = input()
 
 if flor == "acobreada" and (numero % 2 != 0 or (macaneta == "triangular" or macaneta == "quadrada")) and planta == "jiboia":
     pontos += 500
+    acertou5 = "CERTA"
 
 elif cor == "prateada" and (not (flor == "margarida" or flor == "papoula" or flor == "cosmos")) and (macaneta == "hexagonal" or macaneta == "redonda"):
     pontos += 450
+    acertou5 = "CERTA"
 
 elif cor == "dourada" and (flor == "lirio" or flor == "ixora") and macaneta == "hexagonal":
     pontos += 400
+    acertou5 = "CERTA"
 
 else:
     pontos -= 500
+    acertou5 = "ERRADA"
+    acertou_todas = False
 
 #Imprimindo Se ta vivo e sequencia certa
-print("ARISU, VOCÊ FEZ SUAS ESCOLHAS E AGORA VEREMOS SE ESCOLHEU AS PORTAS CERTAS")
+print("ARISU, VOCÊ FEZ SUAS ESCOLHAS E AGORA VEREMOS SE ESCOLHEU AS PORTAS CERTAS:")
+
+print(f"{acertou1} {acertou2} {acertou3} {acertou4} {acertou5}")
+
+if pontos >= 0:
+    if acertou_todas:
+        print(f"Parece que a sorte está ao seu favor, Arisu... Você conseguiu passar com {pontos} pontos!")
+    else:
+        print(f"Você passou com {pontos} pontos, mas faça melhores escolhas da próxima vez.")
+
+else:
+    if pontos > -1400:
+        print(f"Por mais que você tenha feito escolhas corretas, não foi suficiente para sobreviver. Você finalizou o jogo com {pontos} pontos")
+    else:
+        print(f"Todas suas escolhas foram erradas, Arisu, esperávamos mais de você... Você será executado pois obteve {pontos} pontos.")
