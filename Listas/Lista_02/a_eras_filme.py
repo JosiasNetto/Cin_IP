496ab55bd8b15ddd977e4d7789f0b04ee2d69adb
JosiasNetto <josiascaldasnetto08@gmail.com>
2023-11-20 14:45:26 -0300

    Questao a fita

---
+++ Listas/Lista_02/a_eras_filme.py
@@ -0,0 +1,27 @@
+#Variaveis principai para o codigo
+quant_musica = int()
+pontos = int()
+frase = str()
+
+#Recebendo Musica ou frase, e calculando os pontos da plateia dependendo do caso
+while (pontos >= 0 and frase != "long live"):
+    frase = input()
+    if frase == "os fãs estão formando uma ciranda":
+        pontos -= 3 
+
+    elif frase == "os fãs estão ligando os flashes e atrapalhando a visão" or frase == "os fãs estão dançando na frente da tela" or frase == "os fãs estão gritando o nome da Taylor e atrapalhando a música":
+        pontos -= 2
+    
+    elif frase == "os fãs estão cantando as músicas em coro" or frase == "houve um pedido de casamento na sessão":
+        pontos += 2
+    
+    else:
+        pontos += 1
+        quant_musica += 1
+#Imprimindo se o filme foi terminado e a quantidade de musicas tocadas
+else:
+    if pontos < 0:
+       print(f"A Taylor só conseguiu cantar {quant_musica} músicas e a sessão foi interrompida.")
+    
+    elif frase == "long live":
+        print(f"A Taylor conseguiu concluir o show sem muitas interrupções e cantou {quant_musica} músicas.")
