a7d0c6d258e0354858dcb48c6c6333971eea1216
JosiasNetto <josiascaldasnetto08@gmail.com>
2023-11-28 15:55:51 -0300

    Questão 7 consertada

--- Listas/Lista_02/g_plateia_sangue.py
+++ Listas/Lista_02/g_plateia_sangue.py
@@ -2,36 +2,47 @@
 numero_versos = 0
 plateia_nota = 0
 contador_verso = 0
+frase_maiuscula = ""
 
 #Recebendo numero de versos a serem cantados, e fazendo loop dependendo do seu tamanho
 numero_versos = int(input())
 for i in range(numero_versos):
     #Olhando em que verso esta, imprimindo frase e lendo o que a plateia fala, dependendo do verso
+    frase_maiuscula = ""
     if i == 0:
         print("Cause, baby, now we've got")
-        plateia_frase = input().upper()
-        if plateia_frase == "BAD BLOOD":
+        plateia_frase = input()
+        #Loop para gerar frase toda maiuscula a partir do upper de cada caracter 
+        for j in plateia_frase:
+            frase_maiuscula += j.upper()
+        if frase_maiuscula == "BAD BLOOD":
             print("BAD BLOOD")
             plateia_nota += 1
     
     elif i == 1:
         print("You know it used to be")
         plateia_frase = input().upper()
-        if plateia_frase == "MAD LOVE":
+        for j in plateia_frase:
+            frase_maiuscula += j.upper()
+        if frase_maiuscula == "MAD LOVE":
             print("MAD LOVE")
             plateia_nota += 1
 
     elif i == 2:
         print("So take a look what")
         plateia_frase = input().upper()
-        if plateia_frase == "YOU'VE DONE":
+        for j in plateia_frase:
+            frase_maiuscula += j.upper()
+        if frase_maiuscula == "YOU'VE DONE":
             print("YOU'VE DONE")
             plateia_nota += 1
     
     elif i == 3:
         print("Cause, baby, now we've got")
         plateia_frase = input().upper()
-        if plateia_frase == "BAD BLOOD, HEY":
+        for j in plateia_frase:
+            frase_maiuscula += j.upper()
+        if frase_maiuscula == "BAD BLOOD, HEY":
             print("BAD BLOOD, HEY")
             plateia_nota += 1
     
