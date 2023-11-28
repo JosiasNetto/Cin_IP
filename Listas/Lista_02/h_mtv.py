1e75adc6178ec9368d6d4c34a5e7346f3397a4af
JosiasNetto <josiascaldasnetto08@gmail.com>
2023-11-28 20:59:59 -0300

    Questao 8 Finalizada

--- Listas/Lista_02/h_mtv.py
+++ Listas/Lista_02/h_mtv.py
@@ -1,3 +1,4 @@
+#Principais Variaveis do codigo
 numero_celebridades = int(input())
 celebridade_ex_kanye = False
 celebridade_ex_pique = False
@@ -8,7 +9,9 @@ candidato_ariana = False
 candidato_beyonce = False
 candidato_shakira = False
 nome_candidato = "" 
+candidato_ganhador = ""
 
+#Loop que repete dependendo do numero de celebridades, mas so guarda em varial, se alguns especificos aparecerem
 for i in range(numero_celebridades):
     nome_celebridade = input()
     if nome_celebridade == "Kanye West":
@@ -22,6 +25,7 @@ for i in range(numero_celebridades):
     
     print(f"Apresentador: Contamos com a ilustre presença de {nome_celebridade}, uma salva de palmas!")
 
+#Loop que recebe o nome de cada uma das 5 candidatas, e guarda em uma variavel qual compareceu
 while nome_candidato != "Início da Premiação":
     nome_candidato = input()
     if nome_candidato == "Taylor Swift":
@@ -35,4 +39,38 @@ while nome_candidato != "Início da Premiação":
     elif nome_candidato == "Shakira":
         candidato_shakira = True
     
-    
+
+print("Apresentador: Vamos deixar de enrolação e ir para a premiação!")
+
+#Sequencia que confere quem eh a vencedora, baseada na hierarquia do premio. Imprimindo diretamente na checkagem quem ganhou, e vendo se seu ex compareceu para falar
+if candidato_taylor:
+    candidato_ganhador = "TAYLOR SWIFT"
+    print("Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...")
+    print(f"{candidato_ganhador}")
+    if celebridade_ex_kanye:
+        print("Kanye West: Eu vou te deixar terminar. Estou feliz por você, mas Beyoncé fez um dos melhores vídeos de todos os tempos.")
+
+elif candidato_katy:
+    candidato_ganhador = "KATY PERRY"
+    print("Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...")
+    print(f"{candidato_ganhador}")
+
+elif candidato_ariana:
+    candidato_ganhador = "ARIANA GRANDE"
+    print("Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...")
+    print(f"{candidato_ganhador}")
+
+elif candidato_beyonce:
+    candidato_ganhador = "BEYONCÉ"
+    print("Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...")
+    print(f"{candidato_ganhador}")
+    if celebridade_ex_crismartin:
+        print("Chris Martin: Minha heroína, minha irmã, meu tudo. Você merece!")
+
+elif candidato_shakira:
+    candidato_ganhador = "SHAKIRA"
+    print("Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...")
+    print(f"{candidato_ganhador}")
+    if celebridade_ex_pique:
+        print("Gerard Piqué: Meu amor me perdoa, volta pra mim...")
+
