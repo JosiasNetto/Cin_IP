4831871a04fdd4a311220155c1fdd33affdca860
JosiasNetto <josiascaldasnetto08@gmail.com>
2023-11-29 15:44:33 -0300

    Questão 9 Finalizada

--- Listas/Lista_02/i_ex_taylor.py
+++ Listas/Lista_02/i_ex_taylor.py
@@ -1,23 +1,23 @@
+#Recebendo Nome fora do loop(Facilita caso a primeira frase ja seja "vou dormir")
 nome_pretendente = input()
-palavra_pretendente = input()
-palavra_taylor = input()
-palavra_igual = ""
 
-
-while palavra_pretendente != "vou dormir":
+#Loop Para comparar as palavras do pretendente e da taylor, e verificar se são iguais
+while nome_pretendente != "vou dormir":
+    palavra_pretendente = input()
+    palavra_taylor = input()
+    #Loop para verificar se cada caracter do pretendente, é igual a um caracter da palavra da taylor
     for char in palavra_pretendente:
         if char in palavra_taylor:
+            #Se o caracter for igual, retirar da palavra da taylor, esvitando que veja o mesmo caracter mais de uma vez
             palavra_taylor = palavra_taylor.replace(char,"",1)
-        
-        if palavra_taylor == "":
-            print(f"você acertou, estreou na lista! {nome_pretendente}")
-        
-        else:
-            print("perdeu covarde!")
-        
-    nome_pretendente = input()
-    if nome_pretendente != "vou dormir":
-        palavra_pretendente = input()
-        palavra_taylor = input()
 
-        
+    #Checkagem se a palavra do pretendente pode formar a da taylor
+    ##Pois, se tiverer a quantidade suficiente de caracteres iguais, toda a palavra da taylor sera excluida    
+    if palavra_taylor == "":
+        print(f"você acertou, estreou na lista! {nome_pretendente}")
+    
+    else:
+        print("perdeu covarde!")
+
+    #Recebendo o nome do proximo pretendente    
+    nome_pretendente = input()
