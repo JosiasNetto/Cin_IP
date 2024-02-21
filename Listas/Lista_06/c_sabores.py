7cc51bc4988a296ddeb7f9fb71f95f984531b457
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-02-21 16:29:10 -0300

    Questão 03 Lista 06 Iniciada

---
+++ Listas/Lista_06/c_sabores.py
@@ -0,0 +1,4 @@
+comidas = {'bobo de camarao' : ('camarao', 'macaxeira', 'leite de coco', 'dende', 'tomate', 'cebola'), 
+'tapioca de carne de sol' : ('massa de tapioca', 'carne de sol', 'queijo coalho', 'tomate', 'cebola'),
+'carne de sol com macaxeira' : ('carne de sol', 'macaxeira', 'manteiga'), 
+'camarao na moranga' : ('moranga', 'camarao', 'cebola', 'alho', 'tomate', 'pimentao', 'creme de leite', 'azeite', 'coentro')}
